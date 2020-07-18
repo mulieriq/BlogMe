16c371cd60c5cdae58cc3b13da58b0995f6ac8a1
Eric Muli <erickiokom@gmail.com>
2020-07-18 19:27:23 +0300

    Created Post controller

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/','PagesController@index');
 Route::get('/about','PagesController@about');
 Route::get('/services','PagesController@services')->name('services'); /// named route
 
+Route::resource('posts', 'PostsController');
+
+//AUto created by login template
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
